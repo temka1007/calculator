863a4ad128def2e545848dce77022a27b01123ce
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2022-12-01 20:45:48 +0800

    js value given to buttons

---
+++ script.js
@@ -0,0 +1,25 @@
+const display = document.querySelector(".display");
+
+const allCancel = document.querySelector(".all-cancel");
+const cancel = document.querySelector(".cancel");
+const percent = document.querySelector(".percent");
+const divide = document.querySelector(".divide");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const multiply = document.querySelector(".multiply");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const subtract = document.querySelector(".subtract");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const add = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const zero = document.querySelector(".zero");
+const dot = document.querySelector(".dot");
+const equal = document.querySelector(".equal");
+
+
+
